37bd5ce7fc0f08794715e1ead771bf63719dc0a2
Patrycja Dziemianko <patrycja.dziemianko@eclipsegroup.co.uk>
2025-03-11 19:29:46 +0100

    Implement mobile navigation toggle functionality with improved accessibility and styles

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-const mobileMenuButton = document.querySelector(".mobile-nav-toggle");
-const mobileMenu = document.getElementById("primary-navigation");
+const toggleButton = document.querySelector(".mobile-nav-toggle");
+const nav = document.getElementById("primary-navigation");
 
-mobileMenuButton.addEventListener("click", () => {
-  console.log("clicked");
-  mobileMenu.style.transform =
-    mobileMenu.style.transform === "translateX(0%)"
-      ? "translateX(100%)"
-      : "translateX(0%)";
-});
+const toggleNav = () => {
+  const isVisible = nav.getAttribute("data-is-visible") === "true";
+  toggleIcon();
+  nav.setAttribute("data-is-visible", !isVisible);
+};
+
+const toggleIcon = () => {
+  const expanded = toggleButton.getAttribute("aria-expanded") === "true";
+  toggleButton.setAttribute("aria-expanded", !expanded);
+};
+
+toggleButton.addEventListener("click", toggleNav);
