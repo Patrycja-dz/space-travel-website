54b1d5aeefed09a911595ae2d56555a965d57a30
Patrycja Dziemianko <patrycja.dziemianko@eclipsegroup.co.uk>
2025-03-11 19:08:26 +0100

    Implement mobile navigation toggle functionality and update styles - draft

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+const mobileMenuButton = document.querySelector(".mobile-nav-toggle");
+const mobileMenu = document.getElementById("primary-navigation");
 
+mobileMenuButton.addEventListener("click", () => {
+  console.log("clicked");
+  mobileMenu.style.transform =
+    mobileMenu.style.transform === "translateX(0%)"
+      ? "translateX(100%)"
+      : "translateX(0%)";
+});
